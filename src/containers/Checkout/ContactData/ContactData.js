3bd9881053de3e8a9ffeb45b4a31f9037fb11afc
Václav Beneš <pan.vaclav.benes@gmail.com>
2018-06-13 19:47:37 +0200

    redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,12 @@ class ContactData extends Component {
                     type: "text",
                     placeholder: "Your Name"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: "input",
@@ -23,7 +28,12 @@ class ContactData extends Component {
                     type: "text",
                     placeholder: "Street"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: "input",
@@ -31,7 +41,13 @@ class ContactData extends Component {
                     type: "text",
                     placeholder: "ZIP Code"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true,
+                    length: 5,
+                },
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: "input",
@@ -39,7 +55,12 @@ class ContactData extends Component {
                     type: "text",
                     placeholder: "Country"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: "input",
@@ -47,7 +68,12 @@ class ContactData extends Component {
                     type: "email",
                     placeholder: "Email"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: "select",
@@ -57,16 +83,20 @@ class ContactData extends Component {
                         { value: "cheapest", displayValue: "Cheapest" }
                     ]
                 },
+                valid: true,
+                validation: {},
                 value: ""
             },
         },
+        formIsValid: false,
         loading: false
     }
+
 
     orderHandler = (e) => {
         e.preventDefault()
         this.setState({ loading: true })
-        const formData ={}
+        const formData = {}
         for (let formElementIdentifier in this.state.orderForm) {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
         }
@@ -86,12 +116,34 @@ class ContactData extends Component {
             })
     }
 
+    checkValidity(value, rules) {
+        let isValid = true
+
+        if (rules.required) {
+            isValid = value.trim() !== "" && isValid
+        }
+
+        if (rules.length) {
+            isValid = value.length === rules.length && isValid
+        }
+
+        return isValid
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = { ...this.state.orderForm }
         const updatedFormElement = { ...updatedOrderForm[inputIdentifier] }
         updatedFormElement.value = event.target.value
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.touched = true
         updatedOrderForm[inputIdentifier] = updatedFormElement
-        this.setState({orderForm: updatedOrderForm})
+
+        let formIsValid = true
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+        }
+
+        this.setState({ orderForm: updatedOrderForm, formIsValid })
     }
 
     render() {
@@ -111,9 +163,12 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         )
         if (this.state.loading) {
---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+import * as actions from "./actions"
+
+const initialState = {
+    ingredients: [],
+    totalPrice: 0
+}
+
